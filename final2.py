17e8e41691c7d553fa9ab2328b45e16b60f13878
Pierre-Pasquier <60259926+Pierre-Pasquier@users.noreply.github.com>
2023-05-13 19:57:32 +0200

    Add files via upload

--- final2.py
+++ final2.py
@@ -7,7 +7,7 @@ from thymiodirect import Thymio
 
 import numpy as np
 
-import cv2 as cv
+import cv as cv
 
 global balle
 
@@ -78,7 +78,7 @@ id = th.first_node()
 
 def detect_red_ball(image):
     # Convertir l'image en espace de couleur HSV
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
     # Définir les plages de couleur pour le rouge en HSV
     lower_red = np.array([0, 100, 100])
@@ -86,15 +86,15 @@ def detect_red_ball(image):
     upper_red2 = np.array([170, 100, 100])
 
     # Masquer les pixels rouges de l'image
-    mask1 = cv2.inRange(hsv_image, lower_red, upper_red)
-    mask2 = cv2.inRange(hsv_image, upper_red2, upper_red2)
-    mask = cv2.bitwise_or(mask1, mask2)
+    mask1 = cv.inRange(hsv_image, lower_red, upper_red)
+    mask2 = cv.inRange(hsv_image, upper_red2, upper_red2)
+    mask = cv.bitwise_or(mask1, mask2)
 
     # Appliquer une opération de flou pour réduire le bruit
-    blurred = cv2.GaussianBlur(mask, (9, 9), 2)
+    blurred = cv.GaussianBlur(mask, (9, 9), 2)
 
     # Détecter les cercles dans l'image floutée en utilisant la transformée de Hough
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=100, param1=50, param2=30, minRadius=10, maxRadius=100)
+    circles = cv.HoughCircles(blurred, cv.HOUGH_GRADIENT, dp=1, minDist=100, param1=50, param2=30, minRadius=10, maxRadius=100)
 
     if circles is not None:
         # Convertir les coordonnées et le rayon des cercles en entiers
@@ -102,7 +102,7 @@ def detect_red_ball(image):
 
         for (x, y, r) in circles:
             # Dessiner le cercle détecté sur l'image originale
-            cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+            cv.circle(image, (x, y), r, (0, 255, 0), 4)
             partition = 15
             for k in range(1,partition):
                 pix_rouge_bas = image[len(image)-5][k*len(image[0])//partition-1][2]
@@ -130,18 +130,19 @@ def detect_red_ball(image):
 
 
         # Afficher l'image avec les cercles détectés
-        cv2.imshow("Red Ball Detection", image)
+        cv.imshow("Red Ball Detection", image)
     else:
         print("Aucun cercle rouge n'a été détecté.")
         th.set_variable_observer(id, tourner_droite)
         print("Rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien, rien")
 
 # Capture de la vidéo en direct à partir de la caméra
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 while True:
     # Lecture de l'image de la caméra
     ret, image = cap.read()
+    print(ret,cap)
 
     # Vérifier si la capture de la vidéo est réussie
     if not ret:
@@ -151,12 +152,12 @@ while True:
     detect_red_ball(image)
 
     # Quitter la boucle si la touche 'q' est enfoncée
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Libérer les ressources
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
 
 
