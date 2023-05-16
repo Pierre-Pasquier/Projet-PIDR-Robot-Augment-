e1416cb214b70c5fad44923771e1f0a4ada097bf
Pierre-Pasquier <60259926+Pierre-Pasquier@users.noreply.github.com>
2023-05-16 09:16:04 +0200

    Update final4.py

--- final4.py
+++ final4.py
@@ -147,21 +147,21 @@ def retour(node_id):
     if th[node_id]["prox.ground.reflected"][0]< noirsup :
         Passagegauche=1
 
-    if th[node_id]["prox.ground.reflected"][0]> noirsup and Passagegauche=1:
+    if th[node_id]["prox.ground.reflected"][0]> noirsup and Passagegauche==1:
         Passagegauche=2
 
     if th[node_id]["prox.ground.reflected"][1]< noirsup :
         Passagedroit=1
 
-    if th[node_id]["prox.ground.reflected"][1]> noirsup and Passagedroit=1:
+    if th[node_id]["prox.ground.reflected"][1]> noirsup and Passagedroit==1:
         Passagedroit=2
 
         #on dévie à gauche
-    if th[node_id]["prox.ground.reflected"][0]< noirsup and (Passagegauche=2 or Passagedroit=2):
+    if th[node_id]["prox.ground.reflected"][0]< noirsup and (Passagegauche==2 or Passagedroit==2):
         th[node_id]["motor.right.target"] = 0
 
         #on dévie à droite
-    elif th[node_id]["prox.ground.reflected"][1]< noirsup and (Passagegauche=2 or Passagedroit=2):
+    elif th[node_id]["prox.ground.reflected"][1]< noirsup and (Passagegauche==2 or Passagedroit==2):
         th[node_id]["motor.left.target"] = 0
 
     #on est arrivé au bout de la ligne
