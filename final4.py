396ec8517bfdf39ed2eb1dfadebdf9a3027f0fed
Pierre-Pasquier <60259926+Pierre-Pasquier@users.noreply.github.com>
2023-05-16 10:16:15 +0200

    Update final4.py

--- final4.py
+++ final4.py
@@ -133,13 +133,13 @@ def retour(node_id):
 
     #on a trouvé une ligne noire
     if th[node_id]["prox.ground.reflected"][0]< noirsup or th[node_id]["prox.ground.reflected"][1]< noirsup :
-        #if th[node_id]["prox.ground.reflected"][0]< noirsup and th[node_id]["prox.ground.reflected"][1]< noirsup :
-            # DepartLigneNoire += 1
-            # if DepartLigneNoire == 1:
-            #     th[node_id]["motor.left.target"] = 30
-            #     th[node_id]["motor.right.target"] = -30
-            #     print("aaaaaaaaaaaaaa")
-            #     sleep(3)
+        if th[node_id]["prox.ground.reflected"][0]< noirsup and th[node_id]["prox.ground.reflected"][1]< noirsup :
+            DepartLigneNoire += 1
+            if DepartLigneNoire == 1:
+                th[node_id]["motor.left.target"] = 30
+                th[node_id]["motor.right.target"] = -30
+                print("aaaaaaaaaaaaaa")
+                sleep(3)
 
         #print(th[node_id]["prox.ground.reflected"][0])
         #moyen de savoir qu'on viens d'une ligne
