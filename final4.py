580ed4b9db5125c7bf74fc548c51936e95b2546b
Pierre-Pasquier <60259926+Pierre-Pasquier@users.noreply.github.com>
2023-05-16 18:28:32 +0200

    Update final4.py

--- final4.py
+++ final4.py
@@ -150,8 +150,8 @@ def retour(node_id):
                 th[node_id]["motor.right.target"]=Vitesse*3
                 
          if th[node_id]["prox.ground.reflected"][0] > murinf and th[node_id]["prox.ground.reflected"][1] > murinf and SuiviLigne == 1 :
-            th[node_id]["motor.right.target"]= -150
-            sleep(2)
+             th[node_id]["motor.right.target"]= -150
+             sleep(2)
 
 
 
